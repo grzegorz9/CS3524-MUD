787900cc7596604ff6bad2f36db6ff0ebac8820b
grzegorz9 <g.muszynski.12@aberdeen.ac.uk>
2015-03-10 11:59:30 +0000

    Added a batch file for building the project on Windows

---
+++ build.bat
@@ -0,0 +1,9 @@
+javac -cp class -d class .\java\cs3524\mud\Direction.java
+javac -cp class -d class .\java\cs3524\mud\Item.java
+javac -cp class -d class .\java\cs3524\mud\Location.java
+javac -cp class -d class .\java\cs3524\mud\Player.java
+javac -cp class -d class .\java\cs3524\mud\Path.java
+javac -cp class -d class .\java\cs3524\mud\MUD.java
+javac -cp class -d class .\java\cs3524\mud\World.java
+javac -cp class -d class .\java\cs3524\mud\GameClient.java
+javac -cp class -d class .\java\cs3524\mud\GameServer.java
