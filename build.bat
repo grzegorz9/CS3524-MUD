0cfabf2824d9e338478b11a921701e0651639d39
grzegorz9 <g.muszynski.12@aberdeen.ac.uk>
2015-03-12 10:14:20 +0000

    Updated the batch file

--- build.bat
+++ build.bat
@@ -5,5 +5,7 @@ javac -cp class -d class .\java\cs3524\mud\Player.java
 javac -cp class -d class .\java\cs3524\mud\Path.java
 javac -cp class -d class .\java\cs3524\mud\MUD.java
 javac -cp class -d class .\java\cs3524\mud\World.java
+javac -cp class -d class .\java\cs3524\mud\GameSrvrIntfc.java
 javac -cp class -d class .\java\cs3524\mud\GameClient.java
 javac -cp class -d class .\java\cs3524\mud\GameServer.java
+javac -cp class -d class .\java\cs3524\mud\Server.java
